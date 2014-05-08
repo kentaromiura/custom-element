a26dd0a99f6ae8fffefcc44224b6bafa1ac645f8
Cristian Carlesso <kentaromiura@gmail.com>
2014-05-08 09:13:21 +0100

    Space police, Fixes #6

--- custom-element.js
+++ custom-element.js
@@ -7,17 +7,17 @@
       },
       has = (has = Object.prototype.hasOwnProperty, has.call.bind(has)),
       nop = function(){},
-			collect = function(from, collected){
-				if(document.querySelectorAll) return document.querySelectorAll('[data-id]', from)
-				collected = collected || []
-				if('getAttribute' in from && from.getAttribute('data-id')){
-						collected.push(from)
-				}
-				for(var i=0, max = from.childNodes.length; i<max; i++){
-					collect(from.childNodes[i], collected)
-				}
-				return collected
-			}
+      collect = function(from, collected){
+        if (document.querySelectorAll) return document.querySelectorAll('[data-id]', from)
+        collected = collected || []
+        if ('getAttribute' in from && from.getAttribute('data-id')){
+          collected.push(from)
+        }
+        for (var i = 0, max = from.childNodes.length; i<max; i++){
+          collect(from.childNodes[i], collected)
+        }
+        return collected
+      }
 
   document.register.tag = function(name, options){
 
@@ -37,23 +37,23 @@
         delete options.ready;
 
         for (var any in options){
-          if(has(options, any)){
+          if (has(options, any)){
             proto[any] = options[any]
           }
         }
 
       proto.createdCallback = function(){
-				var i, max;
-				this.$ = {}
-        for(i = 0, max = template.childNodes.length; i < max; i++){
+        var i, max;
+        this.$ = {}
+        for (i = 0, max = template.childNodes.length; i < max; i++){
           this.appendChild(template.childNodes[i].cloneNode(true))
         }
-				var shortcuts = collect(this)
-				console.log('shortcuts', shortcuts)
-				for(i = 0, max = shortcuts.length; i < max; i++){
-					var shortcut = shortcuts[i]
-					this.$[shortcut.getAttribute('data-id')] = shortcut
-				}
+        var shortcuts = collect(this)
+
+        for (i = 0, max = shortcuts.length; i < max; i++){
+          var shortcut = shortcuts[i]
+          this.$[shortcut.getAttribute('data-id')] = shortcut
+        }
         ready.call(this)
       }
 
@@ -61,7 +61,6 @@
       prototype : proto
     })
     created.call(registered)
-
   }
 
   var proto = create(HTMLElement.prototype)
@@ -71,19 +70,19 @@
         name = this.getAttribute('name'),
         options
 
-        if(customElements[name] && customElements[name].options){
+        if (customElements[name] && customElements[name].options){
           options = customElements[name].options
         }
         customElements[name] = {
           content: 'content' in template? template.content : template
         }
-        if(options){
+        if (options){
           document.register.tag(name, options)
         }
 
   }
 
-  var injectionPoint = document.head || document.body || document.documentElement || document.createElement('html')
+  var injectionPoint = document.head || document.body || document.documentElement || {appendChild:nop}
   var temp = document.createElement('section');
   temp.innerHTML = '<style>template{display:none!important}/*Added by custom-element*/</style>'
   injectionPoint.appendChild(temp.firstChild)
